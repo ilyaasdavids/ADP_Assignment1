aa00a43f1cb57c09b409beabcc7271f1712310fd
ilyaasdavids <80684345+ilyaasdavids@users.noreply.github.com>
2021-03-30 20:45:32 +0200

    Update Object Equality.java

--- assignment1/Object Equality.java
+++ assignment1/Object Equality.java
@@ -5,7 +5,15 @@ public class ObjectEquality{
     Integer x1 = new Integer( 5 ) ;
     Integer x2 = x1 ;
     Integer x3 = new Integer( 5 ) ;
-    if( x1 == x2 ) System.out.println("x1 eq x2" );
-    if( x2 == x3 ) System.out.println("x2 eq x3" );
+    if(x1.equals(x1)){
+      System.out.println(x1 eq x1);// true
+    }  
+    if(x1.equals(x2)){
+      System.out.println(x1 eq x2);// true
+    }
+
+    if(myCar1.equals(x3){
+      System.out.println(x1 eq x3);// false 
+     }
   }
 }  

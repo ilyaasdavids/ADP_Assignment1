b0738b33e060cb77583387cf9a4a7538adedc6e1
ilyaasdavids <80684345+ilyaasdavids@users.noreply.github.com>
2021-03-30 20:53:51 +0200

    Update and rename Object Identity to Object Identity.java

---
+++ assignment1/Object Identity.java
@@ -0,0 +1,19 @@
+public class ObjectEquality{
+
+  public static void main (String[] args){
+
+    Integer x1 = new Integer( 5 ) ;
+    Integer x2 = x1 ;
+    Integer x3 = new Integer( 5 ) ;
+    if(x1 == x1){
+      System.out.println(x1 eq x1);// true
+    }  
+    if(x1 == x2){
+      System.out.println(x1 eq x2);// true
+    }
+
+    if(x1 == x3){
+      System.out.println(x1 eq x3);// false 
+     }
+  }
+} 

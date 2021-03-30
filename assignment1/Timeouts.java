78ceccf8c9bf023897c219e302454a3f6557b0ab
ilyaasdavids <80684345+ilyaasdavids@users.noreply.github.com>
2021-03-30 22:34:24 +0200

    Update and rename Timeouts to Timeouts.java

---
+++ assignment1/Timeouts.java
@@ -0,0 +1,11 @@
+  
+public class ObjectIdentity{
+  @Test
+  @Timeout(6)
+  public static void main (String[] args){
+  
+  Thread.sleep(5000);
+  System.out.println("Test passed within the time");  
+  
+  }
+} 

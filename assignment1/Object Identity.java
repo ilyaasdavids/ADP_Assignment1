ee4f2c36249483d47b75c1f1a92b246fe96c3422
ilyaasdavids <80684345+ilyaasdavids@users.noreply.github.com>
2021-03-30 22:26:02 +0200

    Update Object Identity.java

--- assignment1/Object Identity.java
+++ assignment1/Object Identity.java
@@ -6,14 +6,14 @@ public class ObjectEquality{
     Integer x2 = x1 ;
     Integer x3 = new Integer( 5 ) ;
     if(x1 == x1){
-      System.out.println(x1 eq x1);// true
+      System.out.println("x1 eq x1");// true
     }  
     if(x1 == x2){
-      System.out.println(x1 eq x2);// true
+      System.out.println("x1 eq x2");// true
     }
 
     if(x1 == x3){
-      System.out.println(x1 eq x3);// false 
+      System.out.println("x1 eq x3");// false 
      }
   }
 } 

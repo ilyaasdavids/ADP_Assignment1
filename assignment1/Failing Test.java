8235799d823f73369e51ab1c75fb9a85c6946c5a
ilyaasdavids <80684345+ilyaasdavids@users.noreply.github.com>
2021-03-31 17:24:14 +0200

    Update Failing Test.java

--- assignment1/Failing Test.java
+++ assignment1/Failing Test.java
@@ -1,1 +1,19 @@
-
+public class FailingTest{
+@Test
+  public void FailTest {
+   
+    Integer x1 = new Integer( 5 ) ;
+    Integer x2 = x1 ;
+    Integer x3 = new Integer( 5 ) ;
+   
+     if(x1 == null) {
+        fail("x1 should not be null");
+     }
+     if(x2 == null) {
+        fail("x2 should not be null");
+     }
+     if(x3 == null) {
+        fail("x3 should not be null");
+     }
+  }
+}  

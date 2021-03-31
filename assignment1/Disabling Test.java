3a9acdc13454199dd83114e7c480d4c10c9e98f0
ilyaasdavids <80684345+ilyaasdavids@users.noreply.github.com>
2021-03-31 17:37:06 +0200

    Update Disabling Test.java

--- assignment1/Disabling Test.java
+++ assignment1/Disabling Test.java
@@ -1,1 +1,13 @@
-@Ignore("not ready yet") @Test public void something() {
+public class DisablingTest{
+@Ignore("not ready yet")  
+@Test
+  public void Disable {
+   
+    Integer x1 = new Integer( 5 ) ;
+    Integer x2 = x1 ;
+    Integer x3 = new Integer( 5 ) ;
+    
+    assertEquals(x1,x1);
+  }
+}
+  
